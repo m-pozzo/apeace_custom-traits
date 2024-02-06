66a55c2d33718b5e374cb66eb022fe1595d2f264
Martin Pozzo <martinpozzo.tin@gmail.com>
2024-02-06 16:56:49 -0300

    new traits and back to JSON

--- js/index.js
+++ js/index.js
@@ -19,203 +19,153 @@ document.addEventListener("DOMContentLoaded", () => {
     const nails = document.getElementById("optionsNails");
     const wrist = document.getElementById("optionsWrist");
     const defaultImg = document.querySelector(".delete");
-
-
-    const traitsData = [
-        {
-            trait: "background",
-            imagen: {
-                black: "../img/background/black.jpg",
-                blue: "../img/background/blue.jpg",
-                orange: "../img/background/orange.jpg"
-            }
-        },
-        {
-            trait: "fur",
-            imagen: {
-                black: "../img/fur/Black.png",
-                blue: "../img/fur/Blue.png",
-                white: "../img/fur/White.png"
-            }
-        },
-        {
-            trait: "clothes",
-            imagen: {
-                admiralsCoat: "../img/clothing/admirals-coat.png",
-                blackHoles: "../img/clothing/black-holes.png",
-                leatherJacket: "../img/clothing/leather-jacket.png"
-            }
-        },
-        {
-            trait: "finger",
-            imagen: {
-                boltRing: "../img/finger/bolt-ring.png",
-                goldRing: "../img/finger/gold-ring.png",
-                woodRing: "../img/finger/wood-ring.png"
-            }
-        },
-        {
-            trait: "hold",
-            imagen: {
-                aceCard: "../img/hold/ace-card.png",
-                banana: "../img/hold/banana.png",
-                fourLeafClover: "../img/hold/four-leaf-clover.png"
-            }
-        },
-        {
-            trait: "nails",
-            imagen: {
-                black: "../img/nails/black.png",
-                hippie: "../img/nails/hippie.png",
-                peace: "../img/nails/peace.png"
-            }
-        },
-        {
-            trait: "wrist",
-            imagen: {
-                chain: "../img/wrist/chain.png",
-                gothicBracelet: "../img/wrist/gothic-bracelet.png",
-                watch: "../img/wrist/watch.png"
-            }
-        }
-    ];
+    const btnDelete = document.createElement('img')
     let arrayBg = [];
 
-    function getTraits() {
-        traitsData.forEach(carc => {
+    async function getTraits() {
+        try {
+            const response = await fetch('../JSON/traits.json');
+            const traits = await response.json()
+            traits.forEach(carc => {
 
-            const trait = carc.trait;
-            const imagen = carc.imagen;
+                const trait = carc.trait;
+                const imagen = carc.imagen;
 
-            for (i = 0; i < 1; i++) {
-                const customImg = document.createElement('img');
-                customImg.id = trait;
-                customImg.classList.add('delete');
-                customImg.style.width = widthAll;
-                customImg.style.height = heightImg;
-                customImg.style.position = "absolute";
-                customImg.style.display = "none"
-                apeace.appendChild(customImg);
+                for (i = 0; i < 1; i++) {
+                    const customImg = document.createElement('img');
+                    customImg.id = trait;
+                    customImg.classList.add('delete');
+                    customImg.style.width = widthAll;
+                    customImg.style.height = heightImg;
+                    customImg.style.position = "absolute";
+                    customImg.style.display = "none"
+                    apeace.appendChild(customImg);
 
-                function zeroRadius() {
-                    if (arrayBg.length === 0) {
-                        // defaultBg.classList.remove("apeace_img"); 
-                        defaultBg.style.borderRadius = "0px";
-                        console.log("se modificó");
-                    } else {
-                        arrayBg.forEach((bg) => {
-                            bg.style.borderRadius = "0px";
-                        });
-                    }
-                }
-
-                for (const color in imagen) {
-                    const urlImg = imagen[color];
-                    const option = document.createElement('img');
-                    option.classList.add('imgOption');
-                    option.src = urlImg;
-                    option.classList.add('option');
-
-                    function insertTrait(width, height, z, trait) {
-                        customImg.classList.add('apeace_img');
-                        customImg.src = urlImg;
-                        customImg.style.width = width;
-                        customImg.style.height = height;
-                        customImg.style.zIndex = z;
-                        customImg.style.display = "block";
-                        if (customImg.id == trait) {
-                            customImg.innerHTML = ``;
+                    function zeroRadius() {
+                        if (arrayBg.length === 0) {
+                            // defaultBg.classList.remove("apeace_img"); 
+                            defaultBg.style.borderRadius = "0px";
+                            console.log("se modificó");
+                        } else {
+                            arrayBg.forEach((bg) => {
+                                bg.style.borderRadius = "0px";
+                            });
                         }
                     }
-                    function deleteTrait(trait) {
-                        const deleteBtn = document.createElement("img");
-                        deleteBtn.src = "../img/x.png";
-                        deleteBtn.style.cursor = "pointer";
-                        trait.childElementCount >= 3 ? trait.append(deleteBtn) : undefined;
-                        deleteBtn.addEventListener('click', () => {
-                            customImg.style.display = "none";
-                            defaultImg.style.display = "none";
-                        });
+
+                    for (const color in imagen) {
+                        const urlImg = imagen[color];
+                        const option = document.createElement('img');
+                        option.classList.add('imgOption');
+                        option.src = urlImg;
+                        option.classList.add('option');
+
+                        function insertTrait(width, height, z, trait) {
+                            customImg.classList.add('apeace_img');
+                            customImg.src = urlImg;
+                            customImg.style.width = width;
+                            customImg.style.height = height;
+                            customImg.style.zIndex = z;
+                            customImg.style.display = "block";
+                            if (customImg.id == trait) {
+                                customImg.innerHTML = ``;
+                            }
+                        }
+                        function deleteTrait(trait) {
+                            // const deleteBtn = document.createElement("img");
+                            // deleteBtn.src = "../img/x.png";
+                            // deleteBtn.style.cursor = "pointer";
+                            // trait.childElementCount >= 22 ? trait.append(deleteBtn) : undefined;
+                            // deleteBtn.addEventListener('click', () => {
+                            //     customImg.style.display = "none";
+                            //     defaultImg.style.display = "none";
+                            // });
+                        }
+                        if (trait === "background") {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthBg, heightImg, "3", "background");
+                                arrayBg.push(customImg);
+                                apeace.removeChild(defaultBg);
+                            });
+                            backgroundColor.append(option);
+                            deleteTrait(backgroundColor);
+                        } else if (trait === "fur") {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthAll, heightImg, "4", "fur");
+                                apeace.removeChild(defaultFur);
+                            });
+                            fur.append(option);
+                            deleteTrait(fur);
+                        } else if (trait === "nails") {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthAll, heightImg, "5", "nails");
+                            });
+                            nails.append(option);
+                            deleteTrait(nails);
+                        } else if (trait === "clothes") {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthAll, heightImg, "10", "clothes");
+                            });
+                            clothes.append(option);
+                            deleteTrait(clothes);
+                        } else if (trait === "finger") {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthAll, heightImg, "15", "finger");
+                            });
+                            finger.append(option);
+                            deleteTrait(finger);
+                        } else if (trait === "hold") {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthAll, heightImg, "20", "hold");
+                            });
+                            holding.append(option);
+                            deleteTrait(holding);
+                        } else {
+                            option.addEventListener('click', () => {
+                                insertTrait(widthAll, heightImg, "25", "wrist");
+                            });
+                            wrist.append(option);
+                            deleteTrait(wrist);
+                        }
                     }
-                    if (trait === "background") {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthBg, heightImg, "3", "background");
-                            arrayBg.push(customImg);
-                            apeace.removeChild(defaultBg);
-                        });
-                        backgroundColor.append(option);
-                        deleteTrait(backgroundColor);
-                    } else if (trait === "fur") {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthAll, heightImg, "4", "fur");
-                            apeace.removeChild(defaultFur);
-                        });
-                        fur.append(option);
-                        deleteTrait(fur);
-                    } else if (trait === "nails") {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthAll, heightImg, "5", "nails");
-                        });
-                        nails.append(option);
-                        deleteTrait(nails);
-                    } else if (trait === "clothes") {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthAll, heightImg, "10", "clothes");
-                        });
-                        clothes.append(option);
-                        deleteTrait(clothes);
-                    } else if (trait === "finger") {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthAll, heightImg, "15", "finger");
-                        });
-                        finger.append(option);
-                        deleteTrait(finger);
-                    } else if (trait === "hold") {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthAll, heightImg, "20", "hold");
-                        });
-                        holding.append(option);
-                        deleteTrait(holding);
-                    } else {
-                        option.addEventListener('click', () => {
-                            insertTrait(widthAll, heightImg, "25", "wrist");
-                        });
-                        wrist.append(option);
-                        deleteTrait(wrist);
+                    createBtn.onclick = () => {
+                        zeroRadius();
+                        apeace.style.width = "1000px";
+                        apeace.style.height = "1000px";
+                        html2canvas(apeace)
+                            .then(function (canvas) {
+                                const imgData = canvas.toDataURL("image/png");
+                                const a = document.createElement('a');
+                                a.download = "yourApeace.png";
+                                a.href = imgData;
+                                a.click();
+                                Toastify({
+                                    text: "¡Created succesfully!",
+                                    duration: 10000,
+                                    style: {
+                                        background: "linear-gradient(to right, #ef972c, #ef972c)",
+                                    }
+                                }).showToast();
+                                apeace.style.width = "420px";
+                                apeace.style.height = "400px";
+                                defaultBg.classList.add("apeace_img");
+                                setTimeout(() => {
+                                    location.reload();
+                                }, 50);
+                            })
+                            .catch((error) => {
+                                console.error("Failed to download the Apeace.", error);
+                            })
                     }
                 }
-                createBtn.onclick = () => {
-                    zeroRadius();
-                    apeace.style.width = "1000px";
-                    apeace.style.height = "1000px";
-                    html2canvas(apeace)
-                        .then(function (canvas) {
-                            const imgData = canvas.toDataURL("image/png");
-                            const a = document.createElement('a');
-                            a.download = "yourApeace.png";
-                            a.href = imgData;
-                            a.click();
-                            Toastify({
-                                text: "¡Created succesfully!",
-                                duration: 10000,
-                                style: {
-                                    background: "linear-gradient(to right, #ef972c, #ef972c)",
-                                }
-                            }).showToast();
-                            apeace.style.width = "420px";
-                            apeace.style.height = "400px";
-                            defaultBg.classList.add("apeace_img");
-                            setTimeout(() => {
-                                location.reload();
-                            }, 50);
-                        })
-                        .catch((error) => {
-                            console.error("Failed to download the Apeace.", error);
-                        })
-                }
-            }
-        });
+            });
+
+        }
+        catch (err) {
+            console.err('An error happend', err)
+        }
     }
+
 
 
 
